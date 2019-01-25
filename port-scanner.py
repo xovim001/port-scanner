c213e249c1da644eb61800390e0ddaa3041c68f8
Sazzad Ovi <40960956+xovim001@users.noreply.github.com>
2019-01-25 20:37:12 +0100

    Add files via upload

---
+++ port-scanner.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python
+import optparse
+import socket
+from socket import *
+from threading import *
+screenLock = Semaphore(value=1)
+
+def banner():
+	print '##########################################'
+	print '##                                      ##'
+	print '##      Author: Sazzad Ovi              ##'
+	print '##                                      ##'
+	print '##           XOVIM001                   ##'
+	print '##                                      ##'
+	print '##                                      ##'
+	print '##########################################'
+	
+banner()
+
+def connScan(tgtHost, tgtPort):
+    try:
+        connSkt = socket(AF_INET, SOCK_STREAM)
+        connSkt.connect((tgtHost, tgtPort))
+        connSkt.send("ViolentPython\r\n")
+        results = connSkt.recv(100)
+        print("[+]%d/tcp open"% tgtPort)
+        print("[+] " + str(results))
+    except:
+        screenLock.acquire()
+        print("[-]%d/tcp closed"% tgtPort)
+    finally:
+        screenLock.release()
+        connSkt.close()
+def portScan(tgtHost, tgtPorts):
+    try:
+        tgtIP = gethostbyname(tgtHost)
+    except:
+        print("[-] Cannot resolve '%s': Unknown host"%tgtHost)
+        return
+    try:
+        tgtName = gethostbyaddr(tgtIP)
+        print("\n[+] Scan Results for: " + tgtName[0])
+    except:
+        print("\n[+] Scan Results for: " +tgtIP)
+    setdefaulttimeout(1)
+    for tgtPort in tgtPorts:
+        t = Thread(target=connScan, args=(tgtHost, int(tgtPort)))
+        t.start()
+def main():
+    parser = optparse.OptionParser("proram usage: " +\
+                                   " -H <target host> -p <target port>")
+    parser.add_option("-H", dest="tgtHost", type="string", \
+help="Please specify a target host")
+    parser.add_option("-p", dest="tgtPort", type="string", \
+                      help="Please specify target port[s] separated by comma ")
+    (options, args) = parser.parse_args()
+    tgtHost = options.tgtHost
+    tgtPorts = str(options.tgtPort).split(",")
+    if (tgtHost == None) | (tgtPorts[0] == None):
+        print(parser.usage)
+        exit(0)
+    portScan(tgtHost, tgtPorts)
+if __name__ == '__main__':
+    main()
+
